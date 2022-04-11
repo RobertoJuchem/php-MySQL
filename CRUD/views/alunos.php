be1f6e71239fa9dcdcad9d83b93036d96b10269e
RobertoJuchem <juchemroberto@gmail.com>
2022-04-11 09:10:44 -0300

    Delete aluno

--- CRUD/views/alunos.php
+++ CRUD/views/alunos.php
@@ -5,17 +5,22 @@
    <tr>
       <th>Nome do Aluno</th>
       <th>Data de Nascimento</th>
+      <th>Delete</th>
    </tr>
 
    <?php
 
-      while($linha = mysqli_fetch_array($consulta_alunos)){
-         echo '<tr>
-                  <td>'.$linha['nome'].'</td>
-                  <td>'.$linha['data_nascimento'].'</td>
-               </tr>';
-      }
+   while ($linha = mysqli_fetch_array($consulta_alunos)) {
+      echo '<tr>
+                  <td>' . $linha['nome'] . '</td>
+                  <td>' . $linha['data_nascimento'] . '</td>';
+   ?>
 
+      <td><a href="deleta_aluno.php?id_aluno=<?php echo $linha['id_aluno']; ?>">🗑️</a></td>
+      </tr></tr>
+
+   <?php
+   }
    ?>
 
 </table>
